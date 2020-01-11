5552f186e8f29a2af53469e6ba1141314d5eccaa
darthnater007 <nsperk@gmail.com>
2020-01-11 02:35:15 -0500

    add git_dict to store returned values, add empty functions to scraper

---
+++ git_dict.py
@@ -0,0 +1,17 @@
+git_dict = {
+	"project_full_path" : "",
+	"branches" : {
+		"current" : "",
+		"shared" : [],
+		"remote" : [],
+	},
+	"commit_diff": {
+		"local_master" : "",
+		"remote_master" : "" ,
+	},
+	"files" : {
+		"staged" : [],
+		"unstaged" : [],
+		"untracked" : [],
+	}
+}
