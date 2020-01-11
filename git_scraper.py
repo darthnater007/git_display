437172970ce76728dde6dc35568c7336da665778
darthnater007 <nsperk@gmail.com>
2020-01-11 02:39:45 -0500

    fix tabs

--- git_scraper.py
+++ git_scraper.py
@@ -1,8 +1,8 @@
 def get_all_branches():
 	# git branch -av
 
-	def get_commit_diff():
-	# https://stackoverflow.com/questions/20433867/git-ahead-behind-info-between-master-and-branch ?? git rev-list <current_branch>..master  (and origin/master for remote) ??
+def get_commit_diff():
+# https://stackoverflow.com/questions/20433867/git-ahead-behind-info-between-master-and-branch ?? git rev-list <current_branch>..master  (and origin/master for remote) ??
 
 def get_file_status():
 	# git status
