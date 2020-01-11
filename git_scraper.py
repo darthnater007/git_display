e6e9485d0185d0e34291793204629224be8e3753
darthnater007 <nsperk@gmail.com>
2020-01-11 18:46:14 -0500

    raw outputs returned in git scraper

--- git_scraper.py
+++ git_scraper.py
@@ -7,9 +7,17 @@ def return_output(command):
 def get_all_branches():
 	raw_output = return_output("git branch -av")
 
+def get_commit_diff():
+	#https://stackoverflow.com/questions/20433867/git-ahead-behind-info-between-master-and-branch
+	local_output_command = "git rev-list --left-right --count " + git_dict.data['branches']['current'] + "..master"
+	remote_output_command = "git rev-list --left-right --count " + git_dict.data['branches']['current'] + "..origin/master"
 
-#def get_commit_diff():
-	# https://stackoverflow.com/questions/20433867/git-ahead-behind-info-between-master-and-branch ?? git rev-list <current_branch>..master  (and origin/master for remote) ??
+	local_raw_output = return_output(local_output_command)
+	remote_raw_output = return_output(remote_output_command)
 
-#def get_file_status():
+	print('local = ' + str(local_raw_output) + '\n\nremote = ' + str(remote_raw_output))
+
+get_commit_diff()
+
+def get_file_status():
 	raw_output = return_output("git status")
