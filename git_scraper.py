434e0d32e1c267573a82dfe470b929355b296605
darthnater007 <nsperk@gmail.com>
2020-01-11 19:07:31 -0500

    remove v flag from git branch command to clean up output

--- git_scraper.py
+++ git_scraper.py
@@ -5,7 +5,7 @@ def return_output(command):
 	return subprocess.run(command, shell=True, stdout=subprocess.PIPE, cwd=git_dict.data["project_full_path"]).stdout
 
 def get_all_branches():
-	raw_output = return_output("git branch -av")
+	raw_output = return_output("git branch -a")
 
 def get_commit_diff():
 	#https://stackoverflow.com/questions/20433867/git-ahead-behind-info-between-master-and-branch
