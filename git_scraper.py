95fece244f2752b15e3bd5ef0d978a341d67d593
darthnater007 <nsperk@gmail.com>
2020-01-12 00:25:50 -0500

    parsing feature-ugly but done

--- git_scraper.py
+++ git_scraper.py
@@ -16,7 +16,6 @@ def get_all_branches():
 	git_dict.data['branches']['local'] = re.findall("\s(?! remotes) ([a-zA-Z_0-9\-]*)", raw_output)
 	git_dict.data['branches']['remote'] = re.findall("remotes/*([^\\\\]*)", raw_output)
 
-
 def get_commit_diff():
 	#https://stackoverflow.com/questions/20433867/git-ahead-behind-info-between-master-and-branch
 	local_output_command = "git rev-list --left-right --count " + git_dict.data['branches']['current'] + "..master"
@@ -28,12 +27,27 @@ def get_commit_diff():
 	git_dict.data['commit_diff']['local_master'] = re.findall("(\d)", local_raw_output)
 	git_dict.data['commit_diff']['remote_master'] = re.findall("(\d)", remote_raw_output)
 
+def strip_status_extras(output):
+	output = output.replace("\\n  (use \"git reset HEAD <file>...\" to unstage)\\n","")
+	output = output.replace("\\n  (use \"git add <file>...\" to update what will be committed)\\n  (use \"git checkout -- <file>...\" to discard changes in working directory)\\n","")
+	output = output.replace("\\n  (use \"git add <file>...\" to include in what will be committed)\\n","")
+	
+	return output
+
+def parse_files(file_string):
+	return re.findall("modified:\s*(\w*.\w*)", file_string)
+
 def get_file_status():
-	raw_output = return_output("git status")
+	raw_output = strip_status_extras(str(return_output("git status")))
+
+	git_dict.data['files']['staged'] = parse_files(re.search("committed:(.*)Changes", raw_output).group(1))
+	git_dict.data['files']['unstaged'] = parse_files(re.search("commit:(.*)Untracked", raw_output).group(1))
+	git_dict.data['files']['untracked'] =  re.findall(r"\\t(\w*.\w*)", re.search("Untracked files:(.*)", raw_output).group(1))
 
 
 
 #test
 get_all_branches()
 get_commit_diff()
+get_file_status()
 print('\n' + json.dumps(git_dict.data, indent=4))
